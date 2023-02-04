b9576fe09d979ccf9c80b44c9fef17724d0a4f9f
linshiyou <2335404839@qq.com>
2023-02-04 23:27:39 +0800

    系统工程

---
+++ xc-plus-system/xc-plus-system-service/src/main/java/com/xc/system/config/config.java
@@ -0,0 +1,33 @@
+package com.xc.system.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlusConfig
+ *
+ * @author: LJ
+ * @create: 2023/2/4
+ */
+@Configuration
+@MapperScan("com.xc.system.mapper")
+public class config {
+
+    /**
+     * 新的分页插件
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
